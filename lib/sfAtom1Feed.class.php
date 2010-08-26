c1c7d1ba4cc238ba1e46cbcb8e6787d441e371c8
pycmam <pycmam@gmail.com>
2010-08-26 22:45:23 +0600

    sfAtom1Feed::getContentType(): убрал кривую отправку Content-type, из-за которой ругались тесты

--- lib/sfAtom1Feed.class.php
+++ lib/sfAtom1Feed.class.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -19,10 +19,13 @@
  */
 class sfAtom1Feed extends sfFeed
 {
+  public function getContentType()
+  {
+    return 'application/atom+xml';
+  }
+
   public function getFeed()
   {
-    header('Content-Type: application/atom+xml');
-
     $xml = array();
     $xml[] = '<?xml version="1.0" encoding="'.$this->getEncoding().'" ?>';
 
